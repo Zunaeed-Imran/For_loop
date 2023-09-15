d569d82e9807cdc7b3e048d673f020deca629cb7
Zunaeed-Imran <zunaeede@gmail.com>
2023-09-15 20:56:59 +0600

    main - let - var - in forloop

---
+++ 2_for_loop.js
@@ -0,0 +1,4 @@
+for (var i = 0; i <= 5; i++){  // if we use let out side for loop not work
+  document.write(i);
+}
+document.write(i); // it will out put only 6 (if we use let would't work)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # For_loop
 For_loop Practice in JavaScript
+2. for loop  
+- if we use var out side for loop will work 
+- if we use let out side for loop will not work
